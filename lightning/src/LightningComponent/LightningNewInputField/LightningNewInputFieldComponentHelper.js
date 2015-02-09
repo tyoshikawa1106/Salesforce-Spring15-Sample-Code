fa8b9734d381151c2900a2d88f335652d64bcca4
Taiki Yoshikawa <tyoshikawa1106@gmail.com>
2015-02-09 23:24:01 +0900

    update

---
+++ lightning/src/LightningComponent/LightningNewInputField/LightningNewInputFieldComponentHelper.js
@@ -0,0 +1,29 @@
+({
+    helperMethod : function(component) {
+        // ui:inputText
+        var inputTextField  = component.find("ui-inputText");
+        var inputText = inputTextField.get("v.value");
+        var outputTextField = component.find("ui-outputText");
+        var outputText = outputTextField.set("v.value", inputText);
+        // ui:inputCurrency
+        var inputCurrencyField  = component.find("ui-inputCurrency");
+        var inputCurrency = inputCurrencyField.get("v.value");
+        var outputCurrencyField = component.find("ui-outputCurrency");
+        var outputCurrency = outputCurrencyField.set("v.value", inputCurrency);
+        // ui:inputEmail
+        var inputEmailField  = component.find("ui-inputEmail");
+        var inputEmail = inputEmailField.get("v.value");
+        var outputEmailField = component.find("ui-outputEmail");
+        var outputEmail = outputEmailField.set("v.value", inputEmail);
+        // ui:inputSecret
+        var inputSecretField  = component.find("ui-inputSecret");
+        var inputSecret = inputSecretField.get("v.value");
+        var outputSecretField = component.find("ui-outputSecret");
+        var outputSecret = outputSecretField.set("v.value", inputSecret);
+        // ui:inputRichText
+        var inputRichTextField  = component.find("ui-inputRichText");
+        var inputRichText = inputRichTextField.get("v.value");
+        var outputRichTextField = component.find("ui-outputRichText");
+        var outputRichText = outputRichTextField.set("v.value", inputRichText);
+    }
+})
